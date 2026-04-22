e49de79027121cf50a9ff637f4bae3eec230ace1
otrutkoav <otrutkoav@gmail.com>
2026-04-22 11:30:23 +0300

    Add project files.

---
+++ TourCore/TourCore/TourCore/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourCore
+{
+    public class Class1
+    {
+    }
+}
